51b5d38b2ed95f9110399f65982a701c4c2b7c78
muhammedfurqan-com <muhammedfurqan@gmail.com>
2025-09-11 17:37:59 +0500

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ import json
 # -------------------------
 # Config / filenames
 # -------------------------
+df = pd.read_excel("Copy of List.xlsx", engine="openpyxl")
 CONFIG_FILE = "app_config.json"
 BACKEND_FILE = "backend_data.csv"
 DEFAULT_CONFIG = {
